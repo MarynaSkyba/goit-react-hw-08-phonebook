12b630f4e42c8d386ea7a827bea4dad5dbb38fbc
Maryna <marynaskyba@gmail.com>
2021-09-04 11:00:49 +0300

    add filter

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@ import { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ContactList from './Contacts/ContactList';
 import ContactForm from './Contacts/ContactForm';
+import Filter from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   formSubmitHandler = (name, number) => {
@@ -15,18 +21,28 @@ export class App extends Component {
       name,
       number,
     };
-    this.setState(prevState => ({
-      contacts: [newContact, ...prevState.contacts],
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
     }));
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
   render() {
+    const normalizedFilter = this.state.filter.toLocaleLowerCase();
+    const activeContacts = this.state.contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter),
+    );
+
     return (
       <div>
         <h2>Phonebook</h2>
-        <ContactForm onSubmit={this.formSubmitHandler}></ContactForm>
+        <ContactForm onSubmit={this.formSubmitHandler} />
         <h2>Contacts</h2>
-        <ContactList contacts={this.state.contacts} />
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactList contacts={activeContacts} />
       </div>
     );
   }
